0dbdcadc8d109b0cce778b4818a2ad3e5d14ed90
andrei <work4program@gmail.com>
2023-05-03 15:29:29 +0700

    внедрение зависмости через контейнер

--- public/index.php
+++ public/index.php
@@ -6,6 +6,12 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
+//$app->config(
+//    [
+//        'DisplayErrorDetails' => true,
+//    ]
+//);
+
 $container = $app->getContainer();
 
 $container['view'] = function ($container) {
@@ -13,12 +19,40 @@ $container['view'] = function ($container) {
     return new \Slim\Views\PhpRenderer('../templates');
 };
 
-$container['configdb'] = require '../config/ConfigDb.php';
+$container['configdb'] = require '../config/db.php';
 
 $container['db'] = function ($container) {
 
     return new \Novokhatsky\DbConnect($container['configdb']);
 };
+
+
+class MyTest
+{
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+
+    public function index($request, $response)
+    {
+
+        $date = $this->db->getValue('select now()');
+
+        $response->getBody()->write("Hello world!" . " now is ${date}");
+        return $response;
+    }
+};
+
+
+$container['MyTest'] = function($container) {
+
+    return new MyTest($container['db']);
+};
+$app->get('/', 'MyTest:index');
 
 $app->run();
 
